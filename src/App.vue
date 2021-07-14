c213024165889cedbebff91feeff0be1118aa238
eyalBarGit <eyalb.media@gmail.com>
2021-07-14 18:12:11 +0300

    show-all page full height

--- src/App.vue
+++ src/App.vue
@@ -33,6 +33,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  height: 100%;
+  height: 100vh;
 }
 </style>
